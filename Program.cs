b5988c1581465967d19aeb0c509fa385039a314f
Jefferson <jsilvardg@outlook.com>
2022-07-06 11:09:55 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundamentsGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
